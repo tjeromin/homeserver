8db373400a7521a5d567da39a020974d8599e868
tjeromin <davidjer@hotmail.de>
2025-02-16 22:53:58 +0100

    script updated to backup folders and db

---
+++ migration/create_backup.sh
@@ -0,0 +1,14 @@
+
+dest=/tmp/nextcloud-dirbkp_`date +"%Y%m%d"`/
+
+printf " --- copy config folder ---\n"
+rsync -Aavx --info=progress2 --info=name0 /var/www/html/config "$dest"
+
+printf "\n\n --- copy themes folder ---\n"
+rsync -Aavx --info=progress2 --info=name0 /var/www/html/themes "$dest"
+
+printf "\n\n --- backup database ---\n"
+read -s -p "Enter password:" password
+printf "\ncreating database dump...\n"
+mysqldump --single-transaction -h localhost -u oc_ncadmin -p"$password" nextcloud > /tmp/nextcloud-dirbkp_`date +"%Y%m%d"`/nextcloud-sqlbkp_`date +"%Y%m%d"`.bak
+
