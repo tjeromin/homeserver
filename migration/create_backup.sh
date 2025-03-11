71db80399457e2b9e9b34afa716e68a9959587a5
tjeromin <davidjer@hotmail.de>
2025-03-11 19:21:15 +0100

    backup influxdb deleted

--- migration/create_backup.sh
+++ migration/create_backup.sh
@@ -14,7 +14,7 @@ printf "\n\n --- copy themes folder ---\n"
 rsync -Aavx --info=progress2 --info=name0 /var/www/html/themes "$dest"
 
 printf "\n\n --- backup database ---\n"
-if mysqldump --single-transaction -h localhost -u root -p nextcloud >"$dest"nextcloud-sqlbkp.bak; then
+if mysqldump --single-transaction -h localhost -u root -p nextcloud >"$dest"nextcloud-sqlbkp.sql; then
     printf "success\n"
 else
     printf "failed\n"
@@ -27,25 +27,6 @@ printf "\n\n--- IOBROKER ---\n"
 
 printf "\n--- copy config folder ---\n"
 rsync -Aavx --info=progress2 --info=name0 /opt/iobroker "$dest"
-
-### INFLUXDB
-
-printf "\n\n--- INFLUXDB ---\n"
-
-mkdir "$dest"influxdb
-
-for bucketid in 1cf0b7166b8afb55 3bf9e01c0e3e7948 c5c943d0ed2227c5 976beebc3c0d1e0a a39b44ee0d3a4e51 cf8589131fc30263; do
-    if influxd inspect export-lp \
-        --bucket-id $bucketid \
-        --engine-path /mnt/drive1/influxdb/engine \
-        --output-path "$dest"influxdb/$bucketid.lp \
-        --compress; then
-        printf "success\n"
-    else
-        printf "failed\n"
-        exit 1
-    fi
-done
 
 ### GRAFANA
 
--- migration/restore_backup.sh
+++ migration/restore_backup.sh
@@ -12,21 +12,29 @@ fi
 printf "\n--- NEXTCLOUD ---\n"
 
 printf "\n--- restore config folder ---\n"
-rsync -Aavx --info=progress2 --info=name0 $1/config /var/www/html/config
+rsync -Aavx --info=progress2 --info=name0 "$1"/config /var/www/html/config
 
 printf "\n\n --- restore themes folder ---\n"
-rsync -Aavx --info=progress2 --info=name0 $1/themes /var/www/html/themes
+rsync -Aavx --info=progress2 --info=name0 "$1"/themes /var/www/html/themes
+
+printf "\n--- IOBROKER ---\n"
+
+printf "\n--- restore config folder ---\n"
+rsync -Aavx --info=progress2 --info=name0 "$1"/iobroker /opt/iobroker
+
+printf "\n\n--- GRAFANA ---\n"
+
+printf "\n--- copy config folders ---\n"
+rsync -Aavx --info=progress2 --info=name0 "$1"/grafana/grafana.ini /etc/grafana/grafana.ini
+rsync -Aavx --info=progress2 --info=name0 "$1"/grafana/custom.ini /etc/grafana/custom.ini
+rsync -Aavx --info=progress2 --info=name0 "$1"/grafana/plugins /var/lib/grafana/plugins
+
+printf "\nfinished\n"
 
 printf "\n\n --- restore database ---\n"
 read -s -p -r "Enter password:" password
 mysql -h localhost -u oc_ncadmin -p"$password" -e "DROP DATABASE nextcloud"
 mysql -h localhost -u oc_ncadmin -p"$password" -e "CREATE DATABASE nextcloud"
 printf "\nrestoring database dump...\n"
-mysql -h localhost -u oc_ncadmin -p"$password" nextcloud < nextcloud-sqlbkp.bak
-
-printf "\n--- IOBROKER ---\n"
-
-printf "\n--- restore config folder ---\n"
-rsync -Aavx --info=progress2 --info=name0 $1/iobroker /opt/iobroker
-
-printf "\nfinished\n"
+printf "\nstart docker-compose file\n"
+docker exec mariadb   sh -c 'mariadb -u root -p"pararas" --database nextcloud < /var/lib/mysql/nextcloud-sqlbkp.sql'
