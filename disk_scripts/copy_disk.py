16774bbe0328b6a7090159447a9ebd5336bdabb7
tjeromin <davidjer@hotmail.de>
2026-04-06 10:59:03 +0000

    added drive3 to backup

--- disk_scripts/copy_disk.py
+++ disk_scripts/copy_disk.py
@@ -1,17 +1,27 @@
 import shutil
 import subprocess
+import sys
+
+if (len(sys.argv) < 3):
+    raise ValueError("Usage: copy_disk.py path_copy_from path_copy_to")
 
 # Define the path and the threshold (100 GB in bytes)
-path = "/mnt/drive1"
-threshold_gb = 100 * (1024**3) 
+path_from = sys.argv[1]
+path_to = sys.argv[2]
+threshold_gb = 100 * (1024**3)
 
 try:
     print("INFO: copy drive1 to drive2...")
 
-    total, used, free = shutil.disk_usage(path)
-    print("total: " + str(total) + " , used: " + str(used) + " , free: " + str(free))
+    total, used, free = shutil.disk_usage(path_from)
+    print("from:\ntotal: " + str(total) + " , used: " + str(used) + " , free: " + str(free))
 
-    if used > threshold_gb:
-        subprocess.run(["sudo", "rsync", "-a", "--delete", "/mnt/drive1/", "/mnt/drive2/"], check=True)
+    total_to, used_to, free_to = shutil.disk_usage(path_to)
+    print("to:\ntotal: " + str(total_to) + " , used: " + str(used_to) + " , free: " + str(free_to))
+
+    if used > threshold_gb and used + 10_000_000_000 > used_to:
+        subprocess.run(["sudo", "rsync", "-a", "--delete", path_from, path_to], check=True)
+    else:
+        raise RuntimeError("less than {threshold_gb} bytes used or from has 10GB less than to. Disk_from probably not mounted.")
 except FileNotFoundError:
-    print(f"Error: {path} not found.")
+    print(f"Error: {path_from} not found.")
